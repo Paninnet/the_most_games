bb1b35a346ef5e2520055cbd3f22ad85cc944fc8
PaninNet <iliapanin300999@mail.ru>
2021-09-08 20:50:53 +0300

    ghp3

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { PostState } from './context/PostState';
-import { BrowserRouter } from 'react-router-dom';
+
 
 
 ReactDOM.render(
-  <BrowserRouter>
     <React.StrictMode>
       <PostState>
         <App />
       </PostState>
-    </React.StrictMode>
-  </BrowserRouter>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
